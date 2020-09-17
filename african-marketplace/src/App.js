e6b8c57a9edf3c9ea8d382f918b6bbe86fff29c6
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-09-16 22:33:16 -0400

    All initial setup are done

--- african-marketplace/src/App.js
+++ african-marketplace/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import LoginCompo from './LoginCompo';
 import NavCompo from './NavCompo';
 import LoginCompo from './LoginCompo';
 import RegisterCompo from './RegisterCompo';
 
-const App = () => {
-    return (
-        <div>
+function App() {
+  return (
+    <div>
             <NavCompo />
             <LoginCompo />
             <RegisterCompo />
         </div>
-    );
-};
+  );
+}
 
 export default App;
