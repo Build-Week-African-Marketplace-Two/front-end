c88a9bcee8f7a57b1aca9c2159914b0bbf556cb3
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-09-24 18:38:44 -0700

    pushing erorr fixes

--- african-marketplace/src/index.js
+++ african-marketplace/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import './sass/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
